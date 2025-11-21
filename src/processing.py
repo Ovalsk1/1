74224555398835043f211dfc55d120e5ebfd9aee
M3 <zaitzew.max2015@yandex.ru>
2025-11-21 20:01:56 +0300

    Added specific type descriptions

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,10 @@
-def filter_by_state(dictionaries_list: list, state="EXECUTED") -> list:
+from datetime import datetime
+from typing import Dict, Any
+
+MIN_DATE = datetime.min  # Минимально возможная дата
+
+
+def filter_by_state(dictionaries_list: list[dict[str, object]], state: str = "EXECUTED") -> list[dict[str, object]]:
     """Функция возвращает отсортированный список словарей."""
     sorted_dictionaries_list = []  # Список подходящих словарей
     for dictionary in dictionaries_list:
@@ -7,19 +13,17 @@ def filter_by_state(dictionaries_list: list, state="EXECUTED") -> list:
     return sorted_dictionaries_list
 
 
-def parse_date(dictionary: dict) -> str:
+def parse_date(dictionary: Dict[str, Any]) -> datetime:
     """Функция для парсинга даты"""
     date_str = dictionary.get("date")  # Получение даты из словаря в списке
     if date_str:
-        return date_str
-    else:
-        return "None"
+        return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
+    return MIN_DATE
 
 
-def sort_by_date(dictionaries_list: list, reverse=True) -> list:
+def sort_by_date(dictionaries_list: list[Dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
     """Функция для сортировки списка по дате"""
-    sorted_dictionaries_list = sorted(dictionaries_list, key=parse_date, reverse=reverse)
-    return sorted_dictionaries_list
+    return sorted(dictionaries_list, key=parse_date, reverse=reverse)
 
 
 dictionaries_list = [
